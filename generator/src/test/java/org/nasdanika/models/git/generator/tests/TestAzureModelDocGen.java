c916bf3d26dce04854be6de56cc2af35a68c97b2
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2025-10-22 06:23:06 -0400

    Updated model doc

--- generator/src/test/java/org/nasdanika/models/git/generator/tests/TestAzureModelDocGen.java
+++ generator/src/test/java/org/nasdanika/models/git/generator/tests/TestAzureModelDocGen.java
@@ -3,7 +3,6 @@ package org.nasdanika.models.git.generator.tests;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -13,41 +12,24 @@ import java.util.function.Function;
 
 import org.eclipse.emf.common.util.DiagnosticException;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EcorePackage;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.junit.jupiter.api.Test;
 import org.nasdanika.common.Context;
 import org.nasdanika.common.Diagnostic;
-import org.nasdanika.common.DiagramGenerator;
 import org.nasdanika.common.ExecutionException;
 import org.nasdanika.common.MutableContext;
-import org.nasdanika.common.NasdanikaException;
 import org.nasdanika.common.NullProgressMonitor;
 import org.nasdanika.common.ProgressMonitor;
-import org.nasdanika.common.Transformer;
-import org.nasdanika.diagramgenerator.plantuml.PlantUMLDiagramGenerator;
-import org.nasdanika.graph.Connection;
-import org.nasdanika.graph.Element;
-import org.nasdanika.graph.emf.EObjectNode;
-import org.nasdanika.graph.processor.NopEndpointProcessorConfigFactory;
-import org.nasdanika.graph.processor.ProcessorConfig;
-import org.nasdanika.graph.processor.ProcessorInfo;
-import org.nasdanika.graph.processor.emf.EObjectNodeProcessorReflectiveFactory;
-import org.nasdanika.html.model.app.Action;
-import org.nasdanika.html.model.app.Label;
-import org.nasdanika.html.model.app.Link;
-import org.nasdanika.html.model.app.gen.ActionSiteGenerator;
-import org.nasdanika.html.model.app.graph.WidgetFactory;
-import org.nasdanika.html.model.app.graph.emf.EObjectReflectiveProcessorFactoryProvider;
+import org.nasdanika.html.bootstrap.Theme;
+import org.nasdanika.models.app.Action;
+import org.nasdanika.models.app.gen.AppSiteGenerator;
 import org.nasdanika.models.azure.AzurePackage;
 import org.nasdanika.models.azure.processors.EcoreGenAzureProcessorsFactory;
-import org.nasdanika.models.ecore.graph.EcoreGraphFactory;
+import org.nasdanika.models.ecore.graph.processors.EcoreHtmlAppGenerator;
 import org.nasdanika.models.ecore.graph.processors.EcoreNodeProcessorFactory;
+import org.nasdanika.ncore.NcorePackage;
+
 
 /**
  * Tests Ecore -> Graph -> Processor -> actions generation
@@ -55,28 +37,11 @@ import org.nasdanika.models.ecore.graph.processors.EcoreNodeProcessorFactory;
  *
  */
 public class TestAzureModelDocGen {
-	
+		
 	@Test
 	public void testGenerateAzureModelDoc() throws IOException, DiagnosticException {
-		List<EPackage> ePackages = Arrays.asList(EcorePackage.eINSTANCE, AzurePackage.eINSTANCE);
 		ProgressMonitor progressMonitor = new NullProgressMonitor(); // new PrintStreamProgressMonitor();
-		Transformer<EObject,Element> graphFactory = new Transformer<>(new EcoreGraphFactory());
-		Map<EObject, Element> graph = graphFactory.transform(ePackages, false, progressMonitor);
-
-		NopEndpointProcessorConfigFactory<WidgetFactory> configFactory = new NopEndpointProcessorConfigFactory<>() {
-			
-			@Override
-			protected boolean isPassThrough(Connection connection) {
-				return false;
-			}
-			
-		};
-		
-		Transformer<Element,ProcessorConfig> processorConfigTransformer = new Transformer<>(configFactory);				
-		Map<Element, ProcessorConfig> configs = processorConfigTransformer.transform(graph.values(), false, progressMonitor);
-		
 		MutableContext context = Context.EMPTY_CONTEXT.fork();
-		context.register(DiagramGenerator.class, new PlantUMLDiagramGenerator());
 		Consumer<Diagnostic> diagnosticConsumer = d -> d.dump(System.out, 0);
 		List<Function<URI,Action>> actionProviders = new ArrayList<>();		
 		EcoreGenAzureProcessorsFactory ecoreGenAzureProcessorFactory = new EcoreGenAzureProcessorsFactory(context);		
@@ -94,90 +59,43 @@ public class TestAzureModelDocGen {
 				diagnosticConsumer,
 				ecoreGenAzureProcessorFactory);
 		
-		EObjectNodeProcessorReflectiveFactory<WidgetFactory, WidgetFactory> eObjectNodeProcessorReflectiveFactory = new EObjectNodeProcessorReflectiveFactory<>(ecoreNodeProcessorFactory);
-		EObjectReflectiveProcessorFactoryProvider eObjectReflectiveProcessorFactoryProvider = new EObjectReflectiveProcessorFactoryProvider(eObjectNodeProcessorReflectiveFactory);
-		Map<Element, ProcessorInfo<Object>> registry = eObjectReflectiveProcessorFactoryProvider.getFactory().createProcessors(configs.values(), false, progressMonitor);
-		
-		WidgetFactory azureProcessor = null;
-		Collection<Throwable> resolveFailures = new ArrayList<>();		
-		URI baseActionURI = URI.createURI("local://azure.models.nasdanika.org/");
-		
-		Map<EPackage, URI> packageURIMap = Map.ofEntries(
-			Map.entry(EcorePackage.eINSTANCE, URI.createURI("https://ecore.models.nasdanika.org/")),			
-			Map.entry(AzurePackage.eINSTANCE, baseActionURI)	
-		);
-		
-		for (EPackage topLevelPackage: ePackages) {
-			for (Entry<Element, ProcessorInfo<Object>> re: registry.entrySet()) {
-				Element element = re.getKey();
-				if (element instanceof EObjectNode) {
-					EObjectNode eObjNode = (EObjectNode) element;
-					EObject target = eObjNode.get();
-					if (target == topLevelPackage) {
-						ProcessorInfo<Object> info = re.getValue();
-						Object processor = info.getProcessor();
-						if (processor instanceof WidgetFactory) {
-							WidgetFactory widgetFactoryNodeProcessor = (WidgetFactory) processor;
-							widgetFactoryNodeProcessor.resolve(packageURIMap.get(topLevelPackage), progressMonitor);
-							
-							if (topLevelPackage == AzurePackage.eINSTANCE) { 							
-								azureProcessor = widgetFactoryNodeProcessor;
-							}
-						}
-					}
-				}
-			}			
-		}
-		
-		if (!resolveFailures.isEmpty()) {
-			NasdanikaException ne = new NasdanikaException("Theres's been " + resolveFailures.size() +  " failures during URI resolution: " + resolveFailures);
-			for (Throwable failure: resolveFailures) {
-				ne.addSuppressed(failure);
-			}
-			throw ne;
-		}								
-		
-		ResourceSet actionModelsResourceSet = new ResourceSetImpl();
-		actionModelsResourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(Resource.Factory.Registry.DEFAULT_EXTENSION, new XMIResourceFactoryImpl());
-		
 		File actionModelsDir = new File("target\\action-models\\");
 		actionModelsDir.mkdirs();
+		File output = new File(actionModelsDir, "azure.xmi");
 		
-		File output = new File(actionModelsDir, "git.xmi");
-		Resource actionModelResource = actionModelsResourceSet.createResource(URI.createFileURI(output.getAbsolutePath()));
-		Collection<Label> labels = azureProcessor.createLabelsSupplier().call(progressMonitor, diagnosticConsumer);
-		for (Label label: labels) {
-			if (label instanceof Link) {
-				Link link = (Link) label;
-				String location = link.getLocation();
-				if (!org.nasdanika.common.Util.isBlank(location)) {
-					URI uri = URI.createURI(location);
-					if (!uri.isRelative()) {
-						link.setLocation("${base-uri}" + uri.deresolve(baseActionURI, true, true, true).toString());
-					}
-				}
-			}
-		}
-						
-		actionModelResource.getContents().addAll(labels);
-		actionModelResource.save(null);
+		
+		Map<EPackage, URI> packageURIMap = Map.ofEntries(
+				Map.entry(EcorePackage.eINSTANCE, URI.createURI("https://ecore.models.nasdanika.org/")),	
+				Map.entry(NcorePackage.eINSTANCE, URI.createURI("https://ncore.models.nasdanika.org/"))	
+			);
+			
+		EcoreHtmlAppGenerator eCoreHtmlAppGenerator = new EcoreHtmlAppGenerator(
+				AzurePackage.eINSTANCE, 
+				packageURIMap, 
+				ecoreNodeProcessorFactory);
+		
+		eCoreHtmlAppGenerator.generateHtmlAppModel(diagnosticConsumer, output, progressMonitor);
 				
 		String rootActionResource = "actions.yml";
 		URI rootActionURI = URI.createFileURI(new File(rootActionResource).getAbsolutePath());//.appendFragment("/");
-		
-		String pageTemplateResource = "page-template.yml";
-		URI pageTemplateURI = URI.createFileURI(new File(pageTemplateResource).getAbsolutePath());//.appendFragment("/");
-		
 		String siteMapDomain = "https://azure.models.nasdanika.org";		
-		ActionSiteGenerator actionSiteGenerator = new ActionSiteGenerator() {
+		AppSiteGenerator actionSiteGenerator = new AppSiteGenerator() {
 			
 			protected boolean isDeleteOutputPath(String path) {
-				return !"CNAME".equals(path);				
+				return !"CNAME".equals(path) && !path.startsWith("demos/");			
 			};
 			
-		};		
+		};
 		
-		Map<String, Collection<String>> errors = actionSiteGenerator.generate(rootActionURI, pageTemplateURI, siteMapDomain, new File("../docs"), new File("target/doc-site-work-dir"), true);
+		URI pageTeplateURI = URI.createFileURI(new File("page-template.yml").getAbsolutePath());//.appendFragment("/");
+		
+		Map<String, Collection<String>> errors = actionSiteGenerator.generate(
+				rootActionURI, 
+				pageTeplateURI, 
+				siteMapDomain, 
+				new File("../docs"), 
+				new File("target/doc-site-work-dir"), 
+				true);
 				
 		int errorCount = 0;
 		for (Entry<String, Collection<String>> ee: errors.entrySet()) {
@@ -190,7 +108,7 @@ public class TestAzureModelDocGen {
 		
 		System.out.println("There are " + errorCount + " site errors");
 		
-		if (errors.size() != 0) {
+		if (errorCount != 322) {
 			throw new ExecutionException("There are problems with pages: " + errorCount);
 		}		
 	}
